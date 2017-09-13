715d838ca529061d865281b6950379842cb72088
ahijevyc <ahijevyc@ucar.edu>
2017-09-13 11:27:56 -0600

    first commit.

---
+++ make_fort.22x.py
@@ -0,0 +1,85 @@
+#!/bin/env python
+from netCDF4 import Dataset
+import numpy as np
+import glob,sys,datetime
+import pdb
+
+# Read u10 and v10 (or slp) from WRF lat-lon files and create fort.222 - fort.224
+#
+# Formatting based on adcirc.org/home/documentation/users-manual-v51/input-file-descriptions/single-file-meteorological-forcing-input-fort-22/ accessed June 15, 2017 and Kate Fossell's experience
+
+# Location of lat-lon files (created with /glade/p/work/ahijevyc/ncl/interpolateWRF.ncl)
+idir = "/glade/scratch/ahijevyc/ADCIRC/IKE/wrf/mem_1/"
+
+# Grid d02 or d03
+grid = sys.argv[1]
+if grid not in ['d01','d02','d03']:
+    print "bad grid",grid
+    sys.exit(1)
+
+# field 'slp' or 'u'
+field = sys.argv[2]
+if field not in ['u','slp']:
+    print "bad field",field
+    sys.exit(1)
+
+search_str= idir+"wrfout_"+grid+"_????-??-??_??:??:??_latlon.nc"
+files = sorted(glob.glob(search_str))
+#print "found", len(files), "files", search_str
+
+# Print array in 8 columns.
+# Sure there is a built-in function for this but I don't know it yet.
+def print_ncols(a,n=80,fill=1013.,fmt="{:10.4f}"):
+    # Replace nans with fill value.
+    a = a.filled(fill)
+    # Print elements from west to east, and then from south to north.
+    # I think that is what order='C' or row-major order does. This is the default.
+    line = ""
+    for i,x in enumerate(a.flatten(order='C')):
+        line = line + fmt.format(x)
+        if len(line) >= n:
+            print line
+            line = ""
+    if line:
+        print line
+
+ncf = Dataset(files[0],"r")
+start_date = getattr(ncf, u'START_DATE')
+yyyymmddhh = start_date[0:4] + start_date[5:7] + start_date[8:10] + start_date[11:13]
+nlat = len(ncf.dimensions['lat'])
+nlon = len(ncf.dimensions['lon'])
+lon = ncf.variables['lon'][:]
+lat = ncf.variables['lat'][:]
+dx = lon[1] - lon[0]
+dy = lat[1] - lat[0]
+ncf.close()
+# valid time should use Time attribute instead of assuming hours since 1901-1-1
+# MAIN HEADER
+print '%55s%10s%5s%10d' % ("",yyyymmddhh,"",2008091406)
+
+for file in files:
+    ncf = Dataset(file,"r")
+    # Sanity check: make sure grid does not change
+    if nlat != len(ncf.dimensions['lat']): sys.exit(1)
+    if nlon != len(ncf.dimensions['lon']): sys.exit(1)
+    lon = ncf.variables['lon'][:]
+    lat = ncf.variables['lat'][:]
+    if dx != lon[1] - lon[0] : sys.exit(1)
+    if dy != lat[1] - lat[0] : sys.exit(1)
+    u10 = ncf.variables['u10'][:]
+    v10 = ncf.variables['v10'][:]
+    slp = ncf.variables['slp'][:]
+    valid_time = datetime.datetime(1901,1,1,0) + datetime.timedelta(hours=float(ncf.variables['Time'][:]))
+    ncf.close()
+    swlon = np.min(lon)
+    # Try making swlon between -180 and 180
+    if swlon >= 180:
+        swlon = swlon - 360.
+    # RECORD HEADER
+    # Does Dt change? Documentation says it is the start time, but Kate says yes.
+    print '%5s%4d%6s%4d%3s%6.4f%3s%6.4f%6s%8.3f%6s%8.3f%3s%12s' % ("iLat=",nlat,"iLong=",nlon,"DX=",dx,"DY=",dy,"SWLat=",np.min(lat),"SWlon=",swlon,"Dt=",valid_time.strftime('%Y%m%d%H%M'))
+    if field == "u":
+        print_ncols(u10,fill=0.,fmt="{:10.5f}")
+        print_ncols(v10,fill=0.,fmt="{:10.5f}")
+    if field == "slp":
+        print_ncols(slp,fill=1013.,fmt="{:10.4f}")
