ee215b3488aba5ab0b297b6a1742c38a794051de
ahijevyc <ahijevyc@ucar.edu>
2018-02-12 11:27:13 -0700

    Added MPAS lat-lon capability. Removed file loop
    Do one files at a time. Because there is a main header and a
    bunch of record headers.
    
    Read MPAS lat-lon file in addition to WRF. Deal with different variable names.

--- make_fort.22x.py
+++ make_fort.22x.py
@@ -1,25 +1,39 @@
 #!/bin/env python
 
-from netCDF4 import Dataset
+from netCDF4 import Dataset, num2date
 import numpy as np
 import glob,sys,datetime
 import pdb
 
-# Read u10 and v10 (or slp) from WRF lat-lon files and create fort.222 - fort.224
+# Read u10 and v10 (or slp) from WRF/MPAS lat-lon files and create fort.222 - fort.224
 #
 """
-The 1st script interpolates to a destination lat-lon grid. Usage:
-ncl /glade/p/work/ahijevyc/ncl/interpolateWRF.ncl
+First interpolate to a destination lat-lon grid.
 
+For WRF: 
+    ncl /glade/p/work/ahijevyc/ncl/interpolateWRF.ncl
+    Produces files like "wrfout_"+grid+"_????-??-??_??:??:??_latlon.nc"
+For MPAS: 
+    ~ahijevyc/bin_cheyenne/mpas_to_latlon
+    Perhaps shave down the longitude range
+    ncks -d lon,500,1200 diag.2017-09-07_00.00.00_0.125deg_000km.nc t.nc
 
-and the 2nd one outputs the fort.22x files.  The fort.22x file could be fort.221 or fort.222 or fort.223 or fort.224, depending on the arguments provided. 1st argument is the grid ("d01", "d02", or "d03"). 2nd argument is "u" or "slp".
+This script produces fort.22x records.  The fort.22x file could be fort.221 or fort.222 or fort.223 or fort.224, depending on the arguments provided.
+1st argument is the file. 2nd argument is "u" or "slp".
 
-Here is the expected string of commands:
+Here is the expected string of commands. You can copy and paste this:
 
-python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py d01 slp > fort.221
-python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py d01 u > fort.222
-python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py d02 slp > fort.223 [optional]
-python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py d02 u > fort.224 [optional]
+# Main header required at top of fort.22x file.
+echo "                                                       2017090700     2017091212" > fort.221
+echo "                                                       2017090700     2017091212" > fort.222
+
+foreach coarse_grid_file (diag*.nc)
+    python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py $coarse_grid_file slp >> fort.221
+    python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py $coarse_grid_file u >> fort.222
+end
+
+python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py nested_grid_file slp >> fort.223 [optional]
+python /glade/p/work/ahijevyc/ADCIRC/make_fort.22x.py nested_grid_file u >> fort.224 [optional]
 
 where d01 is the big domain and d02 is the nested domain.
 
@@ -28,14 +42,7 @@ where d01 is the big domain and d02 is the nested domain.
 
 # Formatting based on adcirc.org/home/documentation/users-manual-v51/input-file-descriptions/single-file-meteorological-forcing-input-fort-22/ accessed June 15, 2017 and Kate Fossell's experience
 
-# Location of lat-lon files (created with /glade/p/work/ahijevyc/ncl/interpolateWRF.ncl)
-idir = "/glade/scratch/ahijevyc/ADCIRC/IRMA/ncar_ensf/2017090800/ens_1/"
-
-# Grid d02 or d03
-grid = sys.argv[1]
-if grid not in ['d01','d02','d03']:
-    print "bad grid",grid
-    sys.exit(1)
+file = sys.argv[1]
 
 # field 'slp' or 'u'
 field = sys.argv[2]
@@ -43,17 +50,12 @@ if field not in ['u','slp']:
     print "bad field",field
     sys.exit(1)
 
-search_str= idir+"wrfout_"+grid+"_????-??-??_??:??:??_latlon.nc"
-files = sorted(glob.glob(search_str))
-if len(files) == 0:
-    print "found", len(files), "files", search_str
-    sys.exit(1)
-
 # Print array in 8 columns.
 # Sure there is a built-in function for this but I don't know it yet.
 def print_ncols(a,n=80,fill=1013.,fmt="{:10.4f}"):
     # Replace nans with fill value.
-    a = a.filled(fill)
+    inds = np.where(np.isnan(a))
+    a[inds] = fill
     # Print elements from west to east, and then from south to north.
     # I think that is what order='C' or row-major order does. This is the default.
     line = ""
@@ -65,43 +67,49 @@ def print_ncols(a,n=80,fill=1013.,fmt="{:10.4f}"):
     if line:
         print line
 
-ncf = Dataset(files[0],"r")
-start_date = getattr(ncf, u'START_DATE')
-yyyymmddhh = start_date[0:4] + start_date[5:7] + start_date[8:10] + start_date[11:13]
-nlat = len(ncf.dimensions['lat'])
-nlon = len(ncf.dimensions['lon'])
-lon = ncf.variables['lon'][:]
-lat = ncf.variables['lat'][:]
-dx = lon[1] - lon[0]
-dy = lat[1] - lat[0]
-ncf.close()
-# valid time should use Time attribute instead of assuming hours since 1901-1-1
-# MAIN HEADER
-print '%55s%10s%5s%10d' % ("",yyyymmddhh,"",2008091406)
 
-for file in files:
+def get_stuff(file):
     ncf = Dataset(file,"r")
-    # Sanity check: make sure grid does not change
-    if nlat != len(ncf.dimensions['lat']): sys.exit(1)
-    if nlon != len(ncf.dimensions['lon']): sys.exit(1)
+    if hasattr(ncf, u'START_DATE'):
+        start_date = getattr(ncf, u'START_DATE')
+    if hasattr(ncf, u'config_start_time'):
+        start_date = getattr(ncf, u'config_start_time')
+    yyyymmddhh = start_date[0:4] + start_date[5:7] + start_date[8:10] + start_date[11:13]
     lon = ncf.variables['lon'][:]
     lat = ncf.variables['lat'][:]
-    if dx != lon[1] - lon[0] : sys.exit(1)
-    if dy != lat[1] - lat[0] : sys.exit(1)
     u10 = ncf.variables['u10'][:]
     v10 = ncf.variables['v10'][:]
-    slp = ncf.variables['slp'][:]
-    valid_time = datetime.datetime(1901,1,1,0) + datetime.timedelta(hours=float(ncf.variables['Time'][:]))
+    if hasattr(ncf, 'model') and ncf.model == "mpas":
+        valid_time, = num2date(ncf.variables['time'][:],ncf.variables['time'].units)
+        # Round up datetime to nearest second. Prevents datetime(2012,12,31,23,22,24,999998) from being 20121231232224. 
+        dsec = np.round(valid_time.microsecond/1000000.)
+        valid_time = valid_time + datetime.timedelta(seconds=dsec)
+        slp = ncf.variables['mslp'][:]
+    else:
+        # valid time should use Time attribute instead of assuming hours since 1901-1-1
+        valid_time = datetime.datetime(1901,1,1,0) + datetime.timedelta(hours=float(ncf.variables['Time'][:]))
+        slp = ncf.variables['slp'][:]
+    if np.max(slp) > 100000: # Convert Pa to hPa
+        slp = slp/100.
     ncf.close()
-    swlon = np.min(lon)
-    # Try making swlon between -180 and 180
-    if swlon >= 180:
-        swlon = swlon - 360.
-    # RECORD HEADER
-    # Does Dt change? Documentation says it is the start time, but Kate says yes.
-    print '%5s%4d%6s%4d%3s%6.4f%3s%6.4f%6s%8.3f%6s%8.3f%3s%12s' % ("iLat=",nlat,"iLong=",nlon,"DX=",dx,"DY=",dy,"SWLat=",np.min(lat),"SWlon=",swlon,"Dt=",valid_time.strftime('%Y%m%d%H%M'))
-    if field == "u":
-        print_ncols(u10,fill=0.,fmt="{:10.5f}")
-        print_ncols(v10,fill=0.,fmt="{:10.5f}")
-    if field == "slp":
-        print_ncols(slp,fill=1013.,fmt="{:10.4f}")
+    return yyyymmddhh, valid_time, lon, lat, u10, v10, slp
+
+yyyymmddhh, valid_time, lon, lat, u10, v10, slp = get_stuff(file)
+
+nlat = lat.size
+nlon = lon.size
+dx = lon[1] - lon[0]
+dy = lat[1] - lat[0]
+
+swlon = np.min(lon)
+# Try making swlon between -180 and 180
+if swlon >= 180:
+    swlon = swlon - 360.
+# RECORD HEADER
+# Does Dt change? Documentation says it is the start time, but Kate says yes.
+print '%5s%4d%6s%4d%3s%6.4f%3s%6.4f%6s%8.3f%6s%8.3f%3s%12s' % ("iLat=",nlat,"iLong=",nlon,"DX=",dx,"DY=",dy,"SWLat=",np.min(lat),"SWlon=",swlon,"Dt=",valid_time.strftime('%Y%m%d%H%M'))
+if field == "u":
+    print_ncols(u10,fill=0.,fmt="{:10.5f}")
+    print_ncols(v10,fill=0.,fmt="{:10.5f}")
+if field == "slp":
+    print_ncols(slp,fill=1013.,fmt="{:10.4f}")
