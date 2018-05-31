f6dbc7fa8feb7f4ddb53f9ae15f785b458022bb3
ahijevyc <ahijevyc@ucar.edu>
2018-05-31 17:07:12 -0600

    added ability to convert multi-time ECMWF

--- make_fort.22x.py
+++ make_fort.22x.py
@@ -7,9 +7,14 @@ import pdb
 
 # Read u10 and v10 (or slp) from WRF/MPAS lat-lon files and create fort.222 - fort.224
 #
+# May 14, 2018 - added ability to convert multi-time ECMWF 
+
 """
 First interpolate to a destination lat-lon grid.
 
+For ECMWF:
+    grib2 files are already on lat/lon grid.
+    Just ncl_convert2nc
 For WRF: 
     ncl /glade/p/work/ahijevyc/ncl/interpolateWRF.ncl
     Produces files like "wrfout_"+grid+"_????-??-??_??:??:??_latlon.nc"
@@ -71,7 +76,11 @@ def print_ncols(a,n=80,fill=1013.,fmt="{:10.4f}"):
             line = ""
     if line:
         print line
-
+def roll(x, lon):
+    # roll array so longitudes start at dateline 
+    pos, = np.where(lon == 180)
+    axis = x.ndim - 1
+    return np.roll(x,pos,axis=axis)
 
 def get_stuff(file):
     if file == 'latlon.nc':
@@ -84,42 +93,76 @@ def get_stuff(file):
         start_date = getattr(ncf, u'START_DATE')
     if hasattr(ncf, u'config_start_time'):
         start_date = getattr(ncf, u'config_start_time')
+    if 'lon' in ncf.variables:
+        lon = ncf.variables['lon'][:]
+        lat = ncf.variables['lat'][:]
+    if 'u10' in ncf.variables:
+        u10 = ncf.variables['u10'][:]
+        v10 = ncf.variables['v10'][:]
+    # Grib file ECMWF
+    if "10u_P1_L103_GLL0" in ncf.variables:
+        sdate = ncf.variables["10u_P1_L103_GLL0"].initial_time
+        start_date = sdate[6:10]+"/"+sdate[0:5]+" "+sdate[12:14]
+        u10s = ncf.variables['10u_P1_L103_GLL0'][:]
+        v10s = ncf.variables['10v_P1_L103_GLL0'][:]
+        slps = ncf.variables['msl_P1_L101_GLL0'][:]
+        lon = ncf.variables['lon_0'][:]
+        lat = ncf.variables['lat_0'][:]
+        u10s = roll(u10s,lon)
+        v10s = roll(v10s,lon)
+        slps = roll(slps,lon)
+        lon  = roll(lon,lon)# make sure you roll longitude last!
+
+
     yyyymmddhh = start_date[0:4] + start_date[5:7] + start_date[8:10] + start_date[11:13]
-    lon = ncf.variables['lon'][:]
-    lat = ncf.variables['lat'][:]
-    u10 = ncf.variables['u10'][:]
-    v10 = ncf.variables['v10'][:]
     if hasattr(ncf, 'model') and ncf.model == "mpas":
         valid_time, = num2date(ncf.variables['time'][:],ncf.variables['time'].units)
         # Round up datetime to nearest second. Prevents datetime(2012,12,31,23,22,24,999998) from being 20121231232224. 
         dsec = np.round(valid_time.microsecond/1000000.)
         valid_time = valid_time + datetime.timedelta(seconds=dsec)
+        valid_times = [valid_time]
         slp = ncf.variables['mslp'][:]
-    else:
+        slps = [slp]
+    if 'slp' in ncf.variables and 'Time' in ncf.variables:
         # valid time should use Time attribute instead of assuming hours since 1901-1-1
         valid_time = datetime.datetime(1901,1,1,0) + datetime.timedelta(hours=float(ncf.variables['Time'][:]))
+        valid_times = [valid_time]
         slp = ncf.variables['slp'][:]
-    if np.max(slp) > 100000: # Convert Pa to hPa
-        slp = slp/100.
-    ncf.close()
-    return yyyymmddhh, valid_time, lon, lat, u10, v10, slp
+        slps = [slp]
+    if 'forecast_time0' in ncf.variables:
+        fhrs = ncf.variables['forecast_time0'][:]
+        valid_times = [datetime.datetime.strptime(yyyymmddhh, '%Y%m%d%H') + datetime.timedelta(hours=int(x)) for x in fhrs]
 
-yyyymmddhh, valid_time, lon, lat, u10, v10, slp = get_stuff(file)
+    if np.max(slps) > 100000: # Convert Pa to hPa
+        slps = slps/100.
+     
+    ncf.close()
+
+    # making lon between -180 and 180
+    lon[lon >= 180] = lon[lon >= 180] - 360.
+
+    if lat[1] - lat[0] < 0:
+        # flip latitude dimension
+        lat  = np.flip(lat, 0)
+        u10s = np.flip(u10s,1)
+        v10s = np.flip(v10s,1)
+        slps = np.flip(slps,1)
+
+    return yyyymmddhh, valid_times, lon, lat, u10s, v10s, slps
+
+yyyymmddhh, valid_times, lon, lat, u10s, v10s, slps = get_stuff(file)
 
 nlat = lat.size
 nlon = lon.size
 dx = lon[1] - lon[0]
 dy = lat[1] - lat[0]
 
-swlon = np.min(lon)
-# Try making swlon between -180 and 180
-if swlon >= 180:
-    swlon = swlon - 360.
 # RECORD HEADER
 # Does Dt change? Documentation says it is the start time, but Kate says yes.
-print '%5s%4d%6s%4d%3s%6.4f%3s%6.4f%6s%8.3f%6s%8.3f%3s%12s' % ("iLat=",nlat,"iLong=",nlon,"DX=",dx,"DY=",dy,"SWLat=",np.min(lat),"SWlon=",swlon,"Dt=",valid_time.strftime('%Y%m%d%H%M'))
-if field == "u":
-    print_ncols(u10,fill=0.,fmt="{:10.5f}")
-    print_ncols(v10,fill=0.,fmt="{:10.5f}")
-if field == "slp":
-    print_ncols(slp,fill=1013.,fmt="{:10.4f}")
+for valid_time, u10, v10, slp in zip(valid_times,u10s,v10s,slps):
+    print '%5s%4d%6s%4d%3s%6.4f%3s%6.4f%6s%8.3f%6s%8.3f%3s%12s' % ("iLat=",nlat,"iLong=",nlon,"DX=",dx,"DY=",dy,"SWLat=",lat[0],"SWlon=",lon[0],"Dt=",valid_time.strftime('%Y%m%d%H%M'))
+    if field == "u":
+        print_ncols(u10,fill=0.,fmt="{:10.5f}")
+        print_ncols(v10,fill=0.,fmt="{:10.5f}")
+    if field == "slp":
+        print_ncols(slp,fill=1013.,fmt="{:10.4f}")
